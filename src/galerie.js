dd0e77815df40ddd365c691f7239d27890bcbdb0
Laura Hetzel <laura@hetzel-family.de>
2018-10-30 12:34:17 +0100

    gemerched

---
+++ src/galerie.js
@@ -0,0 +1,41 @@
+/**Galerie Filteroption Bilder ein/ausblenden**/
+$(document).ready(function(){
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+        }
+        $(".filter-button").removeClass("filter-button-active");
+        $(this).addClass("filter-button-active");
+    });
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+});
+
+
+
+/**Button top **/
+$(document).ready(function(){
+      $('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-chevron-up"></span> Back to Top</div>');
+    	$(window).scroll(function () {
+			if ($(this).scrollTop() != 0) {
+				$('#toTop').fadeIn();
+			} else {
+				$('#toTop').fadeOut();
+			}
+		});
+    $('#toTop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+
+
+    });
+});
