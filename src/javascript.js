3b15425c7aa5a6b1940a2d28bfb11203e3a58e49
Laura Hetzel <laura@hetzel-family.de>
2018-10-30 10:31:55 +0100

    final

--- src/javascript.js
+++ src/javascript.js
@@ -1,4 +1,5 @@
 /*Button top */
+/*schaut ob auf der Seite schon runtergescrollt ist und wenn ja wird der ToTop Button angezeigt*/
 $(document).ready(function(){
       $('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-chevron-up"></span> Back to Top</div>');
     	$(window).scroll(function () {
@@ -8,6 +9,8 @@ $(document).ready(function(){
 				$('#toTop').fadeOut();
 			}
 		});
+
+    /*Wenn auf den Button geklickt wird, wird die Seite ganz nach oben gescrollt*/
     $('#toTop').click(function(){
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
@@ -19,20 +22,20 @@ $(document).ready(function(){
 
 
 /*Einzelvorstellung*/
-
-filterSelection("all") // Execute the function and show all columns
+filterSelection("all")
+/*Ist all angeklickt, werden alle Anzeigt*/
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  //Fügt der gefliterten Klasse die "show" Klasse hinzu und entfernt sie von den anderen Klassen
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
-// Show filtered elements
+// Die gefilterte Klasse wird angezeigt
 function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -44,7 +47,7 @@ function w3AddClass(element, name) {
   }
 }
 
-// Hide elements that are not selected
+// Die nicht ausgewählten Klassen werden nicht angezeigt 
 function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -57,7 +60,7 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
+// Der ausgewählten Klasse wird die Klasse active hinzugefügt
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
@@ -67,3 +70,26 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+/*Browser und Server beinhalten je einen Teil der Anwendungslogik
+•	Der HTML-Code der Seite kann zur Laufzeit generiert werden oder in einer einfachen HTML-Datei liegen
+•	Bei verschiedenen Aktionen werden mit JavaScript weitere Daten mit dem Server ausgetauscht
+•	Die empfangenen Daten werden dabei durch geschickte DOM-Manipulation sichtbar gemacht
+*/
+
+/*Vorteil dieses Ansatzes ist, dass die serverseitigen Funktionen aus
+unterschiedlichen Frontends heraus konsumiert werden können, beispielsweise
+einer Webseite und einer Desktopanwendung. Auch kann eine so entwickelte
+Webanwendung längere Offlinezeiten überstehen, solange sie keine Verbindung zum
+Server benötigt. */
+
+/*•	PHP: Der Vorreiter aller aktiven Serverseiten und das einzige Produkt, dessen Abkürzung keine „Server Pages“ enthält. PHP stand ursprünglich für „Personal Homepage Processor”.
+•	JSP: Java Server Pages, also die Javavariante aktiver Serverseiten
+*/
+
+/*Was der Server bei einer Anfrage macht
+Jeder Aufruf führt zur Auswahl von genau einer URL-Route, die wiederum den
+so genannten Request Handler benennt, der die Anfrage übergeben bekommt.
+Je nach Programmiersprache und Framework handelt es sich dabei entweder um
+eine Klasse (wie in Java, wo diese Klasse Servlet genannt wird), eine Methode
+oder eine Funktion.*/
